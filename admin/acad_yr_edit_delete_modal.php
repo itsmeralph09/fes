847ca6a36f7821e44da7f26b20e4be3141434f56
itsmeralph09 <ralphenigmatic@gmail.com>
2023-09-05 19:04:46 +0800

    Added start and stop button to academic year

--- admin/acad_yr_edit_delete_modal.php
+++ admin/acad_yr_edit_delete_modal.php
@@ -73,6 +73,79 @@
     </div>
 </div>
 
+<!-- Start -->
+<div class="modal fade" id="start_<?php echo $row['acad_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            	<div class="row float-left ml-2"><h4 class="modal-title float-left" id="myModalLabel">Start Academic Year and Semester</h4></div>
+                <div class="row float-right mr-2"><button type="button" class="close float-right" data-dismiss="modal" aria-hidden="true">&times;</button></div>
+            </div>
+            <div class="modal-body">	
+            	<p class="text-center text-success">Are you sure you want to start this academic year and semester?</p>
+				<h5 class="text-center text-secondary">Academic Year: <span class="text-success"><?php echo $row['year_start'].'-'.$row['year_end']; ?></span></h5>
+				<h5 class="text-center text-secondary">Semester: <span class="text-success">
+					<?php
+
+						if ($row['semester'] == 1) {
+							echo '1st Semester';
+						} elseif ($row['semester'] == 2) {
+							echo '2nd Semester';
+						} elseif ($row['semester'] == 3) {
+							echo 'Mid-Year';
+						}
+
+					?>
+						
+					</span></h5>
+				
+			</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa-solid fa-x mr-1"></i>Cancel</button>
+                <a href="acad_yr_delete.php?acad_id=<?php echo $row['acad_id']; ?>" class="btn btn-danger"><i class="fa fa-trash m-1"></i>Yes</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- Start -->
+<div class="modal fade" id="stop_<?php echo $row['acad_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            	<div class="row float-left ml-2"><h4 class="modal-title float-left" id="myModalLabel">Start Academic Year and Semester</h4></div>
+                <div class="row float-right mr-2"><button type="button" class="close float-right" data-dismiss="modal" aria-hidden="true">&times;</button></div>
+            </div>
+            <div class="modal-body">	
+            	<p class="text-center text-danger">Are you sure you want to stop this academic year and semester?</p>
+				<h5 class="text-center text-secondary">Academic Year: <span class="text-danger"><?php echo $row['year_start'].'-'.$row['year_end']; ?></span></h5>
+				<h5 class="text-center text-secondary">Semester: <span class="text-danger">
+					<?php
+
+						if ($row['semester'] == 1) {
+							echo '1st Semester';
+						} elseif ($row['semester'] == 2) {
+							echo '2nd Semester';
+						} elseif ($row['semester'] == 3) {
+							echo 'Mid-Year';
+						}
+
+					?>
+						
+					</span></h5>
+				
+			</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa-solid fa-x mr-1"></i>Cancel</button>
+                <a href="acad_yr_delete.php?acad_id=<?php echo $row['acad_id']; ?>" class="btn btn-danger"><i class="fa fa-trash m-1"></i>Yes</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
 <!-- Delete -->
 <div class="modal fade" id="delete_<?php echo $row['acad_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
