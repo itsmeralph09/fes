df9373e98ec2803c8fa9a8dee96d4fdd4abdcc51
itsmeralph09 <ralphenigmatic@gmail.com>
2023-09-18 22:16:04 +0800

    Added checking if another academic year and semester is started

--- admin/acad_yr_setdefault.php
+++ admin/acad_yr_setdefault.php
@@ -17,7 +17,17 @@
 			mysqli_close($conn);
 			header('location: acad_yr.php');
 			exit;
-		}
+		} 
+
+		$checkStatusQuery2 = "SELECT * FROM acad_yr_tbl WHERE acad_id != '$acad_id'";
+		$checkStatusResult2 = mysqli_query($conn, $checkStatusQuery2);
+		// $row2 = mysqli_fetch_assoc($checkStatusResult2);
+		if (mysqli_num_rows($checkStatusResult2) >= 1) {
+			$_SESSION['error'] = 'Cannot set as default while another academic year and semester is started!';
+			mysqli_close($conn);
+			header('location: acad_yr.php');
+			exit;
+		} 
 
 
 		$sql = "UPDATE acad_yr_tbl
