a5cfcb750e2c962556771a007c835385a2544c1d
itsmeralph09 <ralphenigmatic@gmail.com>
2023-10-08 16:36:47 +0800

    Fixed db connection for seamless hostinger deployment

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,14 +1,23 @@
 <?php
-
 $servername="localhost";
 $uname="root";
 $pass="";
 $dbname="fes_db";
 
 $conn = mysqli_connect($servername, $uname, $pass, $dbname);
+
 if (!$conn) {
-	echo 'Error connecting to database'.mysqli_connect_error($conn);
+
+	$servername="localhost";
+	$uname="u293681336_fes";
+	$pass="Moondrop_09";
+	$dbname="u293681336_fes";
+
+	$conn = mysqli_connect($servername, $uname, $pass, $dbname);
+	
+	if (!$conn) {
+		echo 'Error connecting to database'.mysqli_connect_error($conn);
 }
 
-
+}
 ?>
